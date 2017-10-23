44b86c7d3848c4899a31553d709cb84455ec6966
Aleksandr Bratchikov <sashabratchikov@gmail.com>
2017-10-23 12:03:43 +0300

    get_most_frequent_words function

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+`lang_frequency.py` contains two functions:
+
+`load_data(filepath)` – takes path to text file and returns its content  
+`get_most_frequent_words(text)` – takes string and returns list with ten most frequent words
+
+How to use:
+```bash
+python3 lang_frequency.py <path to file>
+```
+
+Result (10 most frequent words):
+
+```bash
+['this', 'list', 'contains', 'ten', 'most', 'frequent', 'words', 'in', 'descending', 'order']
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,24 @@
+from sys import argv
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as text_file:
+        return text_file.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words_list = text.split()
+    frequencies = {}
+    for word in words_list:
+        if frequencies.get(word):
+            frequencies[word] = frequencies[word] + 1
+        else:
+            frequencies[word] = 1
+    frequencies_list = list(frequencies.items())
+    frequencies_list.sort(key=lambda item: -item[1])
+    return list(map(lambda item: item[0], frequencies_list[:10]))
 
 
 if __name__ == '__main__':
-    pass
+    text = load_data(argv[1])
+    print(get_most_frequent_words(text))
